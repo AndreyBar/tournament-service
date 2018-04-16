387301efa70546bca195ec3090d4289d4c91e7c7
Andrii Barabash <Andrew.Barbsh@gmail.com>
2018-04-16 23:19:41 +0300

    Initial commit

---
+++ src/main/java/com/tournament/TournamentApp.java
@@ -0,0 +1,11 @@
+package com.tournament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournamentApp {
+    public static void main(String[] args) {
+        SpringApplication.run(TournamentApp.class, args);
+    }
+}
